715d080c6b1282b2136a34c9f22f89322a992930
moggishere <aryyoon@gmail.com>
2021-12-07 14:40:47 -0300

    adicionado camadas da tabela tb_categoria

--- loja-games/src/main/java/model/Categoria.java
+++ loja-games/src/main/java/model/Categoria.java
@@ -1,15 +1,19 @@
 package model;
 
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
+@Entity // == CREATE TABLE
+@Table(name = "tb_categoria") // == tb_categoria
 public class Categoria {
     
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY);
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @NotNull
---
+++ loja-games/src/main/java/repository/CategoriaRepository.java
@@ -0,0 +1,17 @@
+package repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import model.Categoria;
+
+import java.util.List;
+
+@Repository
+public interface CategoriaRepository extends JpaRepository<Categoria, Long> {
+
+    public List <Categoria> findAllByTipoContainingIgnoreCase(String Tipo);
+
+    
+    
+}
